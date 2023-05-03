49c7a29903cde32bfe93dbee610307a2ad1be26d
snwflake <vm@snwflake.ml>
2023-05-03 20:45:08 +0200

    add main spaghetti, this includes: - rest api route for auth - db handler - discord bot - rudimentary logging - task runner

---
+++ main.py
@@ -0,0 +1,180 @@
+import asyncio
+import datetime
+import logging
+import logging.handlers
+from os import getenv
+from typing import Any
+
+import uvicorn
+import discord
+from peewee import *
+from discord import app_commands
+from discord.ext import tasks
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from dotenv import load_dotenv
+
+from template import get_html_template
+from exceptions import HTTPException
+from wotapi import WoTAPI
+from enums import Slot1, Slot2
+from user import User
+
+load_dotenv()
+
+app = FastAPI()
+api = WoTAPI(app_id=getenv("__TANKU_APP_ID"), clan_id=getenv("__TANKU_CLAN_ID"))
+db = SqliteDatabase(getenv("__TANKU_DB"))
+db.connect(reuse_if_open=True)
+db.bind([User])
+db.create_tables([User])
+db.close()
+
+logger = logging.getLogger('tanku-bot')
+logger.setLevel(logging.INFO)
+
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', '%Y-%m-%d %H:%M:%S', style='{')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+TEST_GUILD = discord.Object(id=getenv("__TANKU_GUILD_ID"))
+ACTING_USER = getenv("__TANKU_ALLOWED_ACCOUNT_ID")
+
+task_run_time = datetime.time(hour=2, minute=30, tzinfo=datetime.timezone.utc)
+
+class BotClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents) -> None:
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self) -> None:
+        self.extend_token_task.start()
+        self.tree.copy_global_to(guild=TEST_GUILD)
+        await self.tree.sync(guild=TEST_GUILD)
+
+    @tasks.loop(time=task_run_time)
+    async def extend_token_task(self) -> None:
+        acting_user = get_api_user()
+        if not acting_user:
+            print('no user found...skipping')
+        else:
+            try:
+                resp = api.extend_access_token(acting_user)
+            except HTTPException as ex:
+                print(ex)
+
+            """
+            # @TODO
+            # error handling
+            """
+            query = User.update(access_token=resp["access_token"], updated_at=datetime.datetime.now()).where(User.account_id == acting_user.account_id)
+            query.execute()
+
+    @extend_token_task.before_loop
+    async def before_my_task(self):
+        await self.wait_until_ready()
+
+
+intents = discord.Intents.default()
+client = BotClient(intents=intents)
+
+@app.on_event("startup")
+async def startup_event() -> None:
+    asyncio.create_task(client.start(token=getenv("__TANKU_BOT_TOKEN")))
+
+@app.get("/auth")
+async def auth(status: str = "", access_token: str = "", nickname: str = "", account_id: int = 0, expires_at: float = 0) -> Any:
+    if status != "ok" or ("" in [status, access_token, nickname, expires_at] or account_id == 0):
+        return get_html_response(success=False)
+
+    if not api.verify_user_token(access_token, nickname, account_id):
+        return get_html_response(success=False)
+
+    try:
+        with db.atomic():
+            user = User.create(
+                account_id = account_id,
+                nickname = nickname,
+                access_token = access_token,
+                expires_at = datetime.datetime.fromtimestamp(expires_at),
+                updated_at = datetime.datetime.now()
+            )
+            if user:
+                return get_html_response(success=True)
+            else:
+                return get_html_response(success=False)
+    except IntegrityError:
+        return get_html_response(success=False)
+
+
+@client.event
+async def on_ready() -> None:
+    print(f"Logged in as {client.user} (ID: {client.user.id})")
+
+@client.tree.command()
+@app_commands.describe(bonus_code="The bonus code", reward="The rewards or None")
+async def bonus_code(interaction: discord.Interaction, bonus_code: str, reward: str = None) -> None:
+    await interaction.response.send_message(f'<https://eu.wargaming.net/shop/redeem/?bonus_mode={bonus_code}>\n{reward or "no reward specified"}')
+
+@client.tree.command()
+@app_commands.describe(first_booster="Requires 10 online members", second_booster="Requires 15 online members")
+async def booster(interaction: discord.Interaction, first_booster: Slot1, second_booster: Slot2) -> None:
+    await interaction.response.defer()
+    acting_user = get_api_user()
+
+    try:
+        online_members = api.get_online_member_count(acting_user)
+    except HTTPException as ex:
+        print(ex)
+        await interaction.followup.send("Something went wrong, ping snwflake")
+
+    if online_members < 10:
+        await interaction.followup.send("Not enough online members")
+    elif online_members < 15:
+        try:
+            api.activate_clan_booster(acting_user, first_booster.value)
+        except HTTPException as ex:
+            print(ex)
+            await interaction.followup.send("Something went wrong, ping snwflake")
+        else:
+            await interaction.followup.send("Booster activated!")
+    else:
+        try:
+            api.activate_clan_booster(acting_user, first_booster.value, second_booster.value)
+        except HTTPException as ex:
+            print(ex)
+            await interaction.followup.send("Something went wrong, ping snwflake")
+        else:
+            await interaction.followup.send("Booster activated!")
+
+
+@client.tree.command()
+async def test(interaction: discord.Interaction) -> None:
+    await interaction.response.defer()
+    acting_user = get_api_user()
+
+    try:
+        online_members = api.get_online_member_count(acting_user)
+    except HTTPException:
+        await interaction.followup.send("Something went wrong, ping snwflake", ephemeral=True)
+    else:
+        await interaction.followup.send(online_members, ephemeral=True)
+
+
+def get_html_response(success: bool = False) -> HTMLResponse:
+    return HTMLResponse(content=get_html_template(success), status_code=200)
+
+def get_api_user() -> User | bool:
+    try:
+        user = User.get(User.account_id == ACTING_USER) or None
+    except User.DoesNotExist:
+        return None
+    else:
+        return user
+
+# @DEBUG
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8085, reload=True)
